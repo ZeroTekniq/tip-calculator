69466fcd85abbe8fc1787106709f865d43153829
DigitalJsn <92397083+DigitalJsn@users.noreply.github.com>
2026-02-03 14:22:28 -0500

    Finished javascript input error UI and also media queries for tablet and desktop

--- dist/script.js
+++ dist/script.js
@@ -1,13 +1,56 @@
 
 const form = document.querySelector('#form');
+const billInputError = document.querySelector('.bill-input-error');
+const numberOfPeopleError = document.querySelector('.number-of-people-error');
 const buttons = document.querySelectorAll('.tip-button');
 const tipAmount = document.querySelector('.tip');
 const totalAmount = document.querySelector('.total');
+const billLabel = document.querySelector('.bill-label');
 let bill = document.querySelector('#bill');
 let numberOfPeople = document.querySelector('#people');
+const numOfPeopleInputBox = document.querySelector('.num-of-people-input-box');
 const custom = document.querySelector('.custom');
 let resetButton = document.getElementById('reset');
 let percentage = 0;  
+
+
+
+const resetForm = () => {
+    resetButtonAndCards();
+    // Reset values to 0
+        percentage = 0;
+        bill = document.querySelector('#bill').value = '';
+        numberOfPeople = document.querySelector('#people').value = '';
+     // Reinitialize input values
+        bill = document.querySelector('#bill'); 
+        bill.disabled = false;
+        numberOfPeople = document.querySelector('#people');
+        numOfPeopleInputBox.classList.remove('num-of-people-input-box-error');
+        numberOfPeople.disabled = false;
+    // Reset Labels
+        console.log('Form reset');
+}
+
+const resetButtonAndCards = () => {
+    tipAmount.textContent = '$0.00';
+    totalAmount.textContent = '$0.00';
+    // Re-enable buttons
+        buttons.forEach(btn => {
+            btn.disabled = false;
+            btn.classList.remove('inactive');
+            btn.classList.remove('inactive.no-touch');
+        })
+}
+const resetButtonPressed = (button) => {
+    resetButton.addEventListener('click', () => {
+        button.classList.remove('active');
+        resetButton.classList.remove('active');
+        billLabel.style.color = 'var(--clr-grey-500)';
+        billInputError.style.display = 'none';
+        numberOfPeopleError.style.display = 'none';
+        resetForm();
+    })
+}
 
 const disableButtons = () => {
     buttons.forEach(btn => {
@@ -15,37 +58,47 @@ const disableButtons = () => {
         btn.classList.add('inactive');
         btn.classList.add('inactive.no-touch');
     });
+    bill.disabled = true;
+    numberOfPeople.disabled = true;
 }
 
-const resetButtonPressed = (button) => {
-    resetButton.addEventListener('click', () => {
-        // form.reset();
-        button.classList.remove('active');
-        resetButton.classList.remove('active');
-        // Reset values to 0
-        percentage = 0;
-        bill = document.querySelector('#bill').value = '';
-        numberOfPeople = document.querySelector('#people').value = '';
-        tipAmount.textContent = '$0.00';
-        totalAmount.textContent = '$0.00';
-        // Reinitialize input values
-        bill = document.querySelector('#bill');
-        numberOfPeople = document.querySelector('#people');
-        // Re-enable buttons
-        buttons.forEach(btn => {
-            btn.disabled = false;
-        })
-    })
+const billInputIsNotValid = () => {
+    
+    let valid = false;
+    if (bill.value !== '' && (!isNaN(bill.value))) {
+        valid = true;
+    } else {
+        bill.disabled = false;
+        resetButtonAndCards();
+        billInputError.style.display = 'block';
+        console.log('Should have displayed bill error message');
+    }
+    console.log(valid);
+    return valid;
 }
 
 
+const numberOfPeopleIsNotValid = () => {
+    let valid = false;
+    if (numberOfPeople.value !== '' && (!isNaN(numberOfPeople.value))) {
+        valid = true;
+        
+    } else {
+        numberOfPeople.disabled = false;
+        resetButtonAndCards();
+        numberOfPeopleError.style.display = 'block';
+        numOfPeopleInputBox.classList.add('num-of-people-input-box-error');
+        console.log('Please enter a valid number');
+    }
+    console.log(valid);
+    return valid;
+}
 
 const activateButton = (button) => {
     buttons.forEach(btn => {
         btn.classList.remove('active');
-        resetButton.classList.add('active');
-        
     })
+    resetButton.classList.add('active');
     button.classList.add('active');
     disableButtons();
     resetButtonPressed(button);
@@ -54,14 +107,11 @@ const activateButton = (button) => {
 const calcTotalPerPerson = (tipPerPerson, bill, numberOfPeople) => {
     const total = tipPerPerson + bill / numberOfPeople;
     totalAmount.textContent = '$' + total.toFixed(2);
-    console.log(total);
 }
 
 const calcTipAmount = (percentage, bill, numberOfPeople) => {
     const tip = bill * percentage;
-    console.log(tip);
     const tipPerPerson = tip / numberOfPeople;
-    console.log(tipPerPerson );
     tipAmount.textContent = '$' + tipPerPerson.toFixed(2);
     calcTotalPerPerson(tipPerPerson, bill, numberOfPeople);
 }
@@ -70,7 +120,6 @@ const convertToNumbers = () => {
    percentage = parseFloat(percentage / 100);
    bill = parseFloat(bill.value);
    numberOfPeople = parseFloat(numberOfPeople.value);
-   console.log(percentage, bill, numberOfPeople);
    calcTipAmount(percentage, bill, numberOfPeople);
 }
   
@@ -84,12 +133,24 @@ const getPercentageNumber = () => {
                 'custom': custom
             };
     buttons.forEach(button => {
+        
         button.addEventListener('click', () => {
             activateButton(button);
-            buttons.disabled = true;
-            percentage = option[button.dataset.option];
-            console.log(percentage, bill, numberOfPeople);
-            convertToNumbers(percentage);
+            if (numberOfPeopleIsNotValid() === false && billInputIsNotValid() === false) {
+                resetButtonPressed(button);
+                return;
+            } 
+            else if (numberOfPeopleIsNotValid() === false || billInputIsNotValid() === false) {
+                resetButtonAndCards();
+                resetButtonPressed(button);
+                return;
+            }
+             else {
+                buttons.disabled = true;
+                percentage = option[button.dataset.option];
+                convertToNumbers(percentage);
+            }
+            
         })
     })
 }
